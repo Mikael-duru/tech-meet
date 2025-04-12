3055a4a3afba84288a181afabc7a23300ff7a0f1
Duru C. Michael <durumikaelc@gmail.com>
2025-04-12 17:09:37 +0100

    complete convex functions

--- convex/users.ts
+++ convex/users.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const syncUser = mutation({
 	args: {
@@ -20,5 +20,43 @@ export const syncUser = mutation({
 			...args,
 			role: "candidate",
 		});
+	},
+});
+
+// // Update user role
+// export const updateUserRole = mutation({
+// 	args: {
+// 		id: v.id("users"),
+// 		role: v.union(v.literal("candidate"), v.literal("interviewer"),),
+// 	},
+// 	handler: async (ctx, args) => {
+// 		return await ctx.db.patch(args.id, {
+// 			role: args.role,
+// 		});
+// 	},
+// });
+
+export const getUsers = query({
+	handler: async (ctx) => {
+		// Check if the user is authenticated
+		const identity = await ctx.auth.getUserIdentity();
+		if (!identity) throw new Error("Unauthorized");
+
+		const users = await ctx.db.query("users").collect();
+		return users;
+	},
+});
+
+export const getUserByClerkId = query({
+	args: {
+		clerkId: v.string(),
+	},
+	handler: async (ctx, args) => {
+		const user = await ctx.db
+			.query("users")
+			.withIndex("by_clerk_id", (q) => q.eq("clerkId", args.clerkId))
+			.first();
+
+		return user;
 	},
 });
