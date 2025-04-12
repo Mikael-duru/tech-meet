cca8b31259ce2188f586715c27f1a68b47e5aad9
Duru C. Michael <durumikaelc@gmail.com>
2025-04-12 14:31:59 +0100

    set up clerk and convex, sync database with clerk using webhook

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+	providers: [
+		{
+			domain: "https://proper-panda-58.clerk.accounts.dev",
+			applicationID: "convex",
+		},
+	],
+};
