17eb2faa9e907abbc71c53b68d9e47987a88e02a
Duru C. Michael <durumikaelc@gmail.com>
2025-04-13 18:49:32 +0100

    use next-themes for clerk theme

--- src/components/providers/convex-clerk-provider.tsx
+++ src/components/providers/convex-clerk-provider.tsx
@@ -3,13 +3,22 @@
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { useTheme } from "next-themes";
+import { dark } from "@clerk/themes";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const ConvexClerkProvider = ({ children }: { children: React.ReactNode }) => {
+	const { theme } = useTheme();
+
+	const clerkModeToggle = theme === "light" ? undefined : dark;
+
 	return (
 		<ClerkProvider
 			publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+			appearance={{
+				baseTheme: clerkModeToggle,
+			}}
 		>
 			<ConvexProviderWithClerk client={convex} useAuth={useAuth}>
 				{children}
